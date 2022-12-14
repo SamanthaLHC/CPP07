5f153e0b56175b06baff84775b2e4d81a0d9a61c
Samantha Le Huec <s.lhuec@gmail.com>
2022-12-14 18:59:28 +0100

    add du main du sujet

--- ex00/srcs/main.cpp
+++ ex00/srcs/main.cpp
@@ -82,4 +82,19 @@ int main()
 	std::cout << str2 << std::endl;
 	std::cout << BBLU << "max val between: " << str1 << " and " << str2 << RES << std::endl;
 	std::cout << max(str1, str2) << std::endl;
+	{
+		std::cout << BRED << "main du sujet" << RES << std::endl;
+		int a = 2;
+		int b = 3;
+		::swap(a, b);
+		std::cout << "a = " << a << ", b = " << b << std::endl;
+		std::cout << "min( a, b ) = " << ::min(a, b) << std::endl;
+		std::cout << "max( a, b ) = " << ::max(a, b) << std::endl;
+		std::string c = "chaine1";
+		std::string d = "chaine2";
+		::swap(c, d);
+		std::cout << "c = " << c << ", d = " << d << std::endl;
+		std::cout << "min( c, d ) = " << ::min(c, d) << std::endl;
+		std::cout << "max( c, d ) = " << ::max(c, d) << std::endl;
+	}
 }
