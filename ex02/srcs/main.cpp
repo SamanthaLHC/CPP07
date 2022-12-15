18821e51229c7f17972c01839bc3132651ce72a9
Samantha Le Huec <s.lhuec@gmail.com>
2022-12-15 17:50:48 +0100

    init ex02

---
+++ ex02/srcs/main.cpp
@@ -0,0 +1,25 @@
+#include "colors.h"
+#include "iter.hpp"
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+void capitalizer(std::string& elem)
+{
+	int size = elem.length();
+	for (int i = 0; i < size ; i++)
+		elem[i] = std::toupper(elem[i]);
+}
+template <>
+void transform<std::string>(std::string& elem)
+{
+	capitalizer(elem);
+}
+
+int main()
+{
+	int arr[5] = {1, 2, 3, 4, 5};
+	test(arr);
+	std::string arr1[5] = {"Hola", "yOupi", "pouet", "coucou", "bisous Maman"};
+	test(arr1);
+}
